1f590a4f2e585a55c6955d9fa8697a87f660e4b1
baozhoutao <baozhoutao@hotoa.com>
2020-08-02 16:46:11 +0800

    如果当前用户只有一个space, 且不能创建space 。则自动进入 #101

--- webapp/src/client/index.js
+++ webapp/src/client/index.js
@@ -75,6 +75,10 @@ const LoginAfter = async (history, tenant, result, location, action)=>{
       if(!user.name){
         return LoginAfterHistoryPush(history, '/set-name' + location.search);
       }else {
+        if(!tenant.enable_create_tenant && user.spaces.length == 1){
+          return goInSystem(history, location, result.tokens.accessToken);
+        }
+
         if(user.spaces.length > 0){
           return LoginAfterHistoryPush(history, '/choose-tenant' + location.search);
         }
